9051783da349ed6c4c3f1f874205d0ab78bcb532
TimJoePat <TimJoePat88@cox.net>
2024-04-04 17:31:36 -0700

    75 - Receiving FormData Complete

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,5 +1,8 @@
-'use server';
+"use server";
 
-export async function createTopic() {
+export async function createTopic(formData: FormData) {
+  const name = formData.get("name");
+  const description = formData.get("description");
   // TODO: revalidate the homepage
+  console.log(name, description);
 }
